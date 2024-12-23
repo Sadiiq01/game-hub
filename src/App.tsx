04489d0d2a7bcf9cee947c2e47eb7bebe23561f6
sadiiq mukhtaar <sadiiqmukhtaar@gmail.com>
2024-12-23 07:47:48 +0300

    creating responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
+import { Grid, GridItem, Show, useBreakpointValue } from "@chakra-ui/react";
 
 const App = () => {
+  const isLargeScreen = useBreakpointValue({ base: false, lg: true });
   return (
     <div>
-      <Button>Click me</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main" `,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area={"nav"} bg={"blue"}>
+          nav
+        </GridItem>
+        <Show when={isLargeScreen}>
+          <GridItem area={"aside"} bg={"gold"}>
+            aside
+          </GridItem>
+        </Show>
+        <GridItem area={"main"} bg={"green"}>
+          main
+        </GridItem>
+      </Grid>
     </div>
   );
 };
