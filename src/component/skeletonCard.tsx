4c6875ac09a5e2a840d90a4638ca7d16dff1ce8f
sadiiq mukhtaar <sadiiqmukhtaar@gmail.com>
2024-12-27 09:38:45 +0300

    solving the issue of key

--- src/component/skeletonCard.tsx
+++ src/component/skeletonCard.tsx
@@ -2,7 +2,7 @@ import { Card, Skeleton } from "@chakra-ui/react";
 
 const SkeletonCard = () => {
   return (
-    <Card.Root width={"300px"} spaceX="40px" gap="40px">
+    <Card.Root width={"100%"} spaceX="40px" gap="40px">
       <Skeleton height="200px" width={"100%"} />
       <Card.Body>
         <Skeleton height="20px" width="100%" />
