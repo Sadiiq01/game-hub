4d6b334bf301e145ad0748e5735d0865f89c4079
sadiiq mukhtaar <sadiiqmukhtaar@gmail.com>
2024-12-28 10:16:08 +0300

    build search box

---
+++ src/component/searchInput.tsx
@@ -0,0 +1,9 @@
+import { InputGroup } from "@/components/ui/input-group";
+import { Input } from "@chakra-ui/react";
+import React from "react";
+
+const SearchInput = () => {
+  return <Input borderRadius={20} placeholder="search gamem..." />;
+};
+
+export default SearchInput;
