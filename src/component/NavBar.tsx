4ca59e49035226007a8b6e52a67b0d5aed8ba2f4
sadiiq mukhtaar <sadiiqmukhtaar@gmail.com>
2024-12-28 10:44:59 +0300

    undo search box

--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -1,14 +1,12 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import MySwitchComponent from "./swithColorMode";
-import SearchInput from "./searchInput";
 
 const NavBar = () => {
   return (
     <div>
       <HStack width={"100%"}>
         <Image src={logo} boxSize={"60px"} />
-        <SearchInput />
         <MySwitchComponent />
       </HStack>
     </div>
--- src/component/searchInput.tsx
+++
@@ -1,9 +0,0 @@
-import { InputGroup } from "@/components/ui/input-group";
-import { Input } from "@chakra-ui/react";
-import React from "react";
-
-const SearchInput = () => {
-  return <Input borderRadius={20} placeholder="search gamem..." />;
-};
-
-export default SearchInput;
